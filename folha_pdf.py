f52b9394f75fe9d00f6726f52f4acb408e69603f
Lara Guerreiro <laraguerreirop@gmail.com>
2022-10-20 09:08:23 +0000

    add more logs

--- folha_pdf.py
+++ folha_pdf.py
@@ -1,4 +1,5 @@
 """Script to get editions from folha de são paulo newspaper"""
+import gc
 import glob
 import pickle
 import shutil
@@ -79,6 +80,7 @@ def get_pages(url_reader, path):
             with open(img_filename,'wb') as file:
                 shutil.copyfileobj(img_content.raw, file)
             page_file_names.append(img_filename)
+            print(f'Downloaded page {page_number} of {len(pages)}')
         else:
             print(f'error downloading page {page_number}')
         page_number = page_number + 1
@@ -112,6 +114,7 @@ def get_pdf(date):
     pdf_filename = f'{path_pdf}/F_{date.strftime("%Y_%m_%d")}.pdf'
     list_of_files = sorted( filter( lambda x: os.path.isfile(os.path.join(temp_path, x)),
                         os.listdir(temp_path) ) )
+    print (f'Starting PDF {pdf_filename}')
     for page_file_name in list_of_files:
         path = os.path.join(temp_path, page_file_name)
         if os.path.isdir(path):
@@ -119,7 +122,10 @@ def get_pdf(date):
         imgs.append(path)
         with open(pdf_filename,"wb") as f:
             f.write(img2pdf.convert(imgs))
+    print (f'Finished PDF {pdf_filename}')
+    print (f'Removing tmp path {temp_path}')
     shutil.rmtree(temp_path)
+    print (f'Removed tmp path {temp_path}')
     print(f'Finished edition {date}')
     
 
@@ -145,8 +151,9 @@ URL_NEWSPAPER = "https://acervo.folha.com.br"
 
 browser = get_browser()
 authentication()
-periods = ["2021-01-01/2021-01-03"]
+periods = ["2021-03-30/2021-12-31"]
 for period in periods:
+    gc.collect()
     period_array = period.split('/')
     start = datetime.fromisoformat(period_array[0])
     end = datetime.fromisoformat(period_array[1])
